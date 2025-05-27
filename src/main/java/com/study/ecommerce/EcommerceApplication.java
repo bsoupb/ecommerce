04fbe67680ddcaa5a15fbe24415932e8bb187072
bsoupb <qotn0327@naver.com>
2025-05-27 14:41:38 +0900

    기본세팅

--- src/main/java/com/study/ecommerce/EcommerceApplication.java
+++ src/main/java/com/study/ecommerce/EcommerceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+//@EnableJpaAuditing 필수
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class EcommerceApplication {
